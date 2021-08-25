984d4f43fc5b89ed11c6402e4f68a3dc754481b7
Mateus Henrique <mhenriqq32@gmail.com>
2021-08-25 19:46:50 -0300

    alpha 3.0

--- services/socketio.js
+++ services/socketio.js
@@ -1,7 +1,5 @@
 import socketIOClient from 'socket.io-client'
 
-const socket = socketIOClient("http://localhost:4000/")
-
-/* https://scuba-senha.herokuapp.com/ */
+const socket = socketIOClient("https://scuba-senha.herokuapp.com/");
 
 export default socket;
